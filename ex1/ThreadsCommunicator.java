0f42622ed69c4f0f6e80bd1bc2a0357ddc5bbdf8
Or Troyaner <or.troyaner@gmail.com>
2017-11-13 18:47:53 +0200

    calcNumOfNeighbourThreads function added

--- ex1/ThreadsCommunicator.java
+++ ex1/ThreadsCommunicator.java
@@ -9,18 +9,22 @@ import static ex1.Message.Direction.*;
 public class ThreadsCommunicator {
     /* holds a bank of messages for thread's produce-consume */
     private MessagesBank[][] messagesBank;
+    private int vSplit;
+    private int hSplit;
 
     public ThreadsCommunicator(int vSplit, int hSplit) {
-        messagesBank = new MessagesBank[vSplit][hSplit];
+        this.vSplit = vSplit;
+        this.hSplit = hSplit;
+        messagesBank = new MessagesBank[this.vSplit][this.hSplit];
         /* initiation of empty banks to the banks matrix */
-        for (int i = 0; i < vSplit; i++) {
-            for (int j = 0; j < hSplit; j++) {
+        for (int i = 0; i < this.vSplit; i++) {
+            for (int j = 0; j < this.hSplit; j++) {
                 messagesBank[i][j] = new MessagesBank();
             }
         }
     }
 
-    public Message getMessageFromBank(int row, int col, int generation)  {
+    public Message getMessageFromBank(int row, int col, int generation) {
         return messagesBank[row][col].getAndRemoveMessage(generation);
     }
 
@@ -33,18 +37,70 @@ public class ThreadsCommunicator {
                 break;
             case RIGHT:
                 messageToInsert.setDirection(LEFT);
-                messagesBank[fromThreadRow][fromThreadCol+1].insertMessage(messageToInsert);
+                messagesBank[fromThreadRow][fromThreadCol + 1].insertMessage(messageToInsert);
                 break;
             case DOWN:
                 messageToInsert.setDirection(UP);
-                messagesBank[fromThreadRow+1][fromThreadCol].insertMessage(messageToInsert);
+                messagesBank[fromThreadRow + 1][fromThreadCol].insertMessage(messageToInsert);
                 break;
             case LEFT:
                 messageToInsert.setDirection(RIGHT);
-                messagesBank[fromThreadRow][fromThreadCol-1].insertMessage(messageToInsert);
+                messagesBank[fromThreadRow][fromThreadCol - 1].insertMessage(messageToInsert);
                 break;
             default:
                 break;
         }
+    }
+
+    public int calcNumOfNeighbourThreads(int threadRow, int threadCol) {
+        if (isThreadIsCorner(threadRow, threadCol)) return 3;
+        if (isThreadOnEdge(threadRow, threadCol)) return 5;
+        return 8;
+    }
+
+    private boolean isThreadOnEdge(int threadRow, int threadCol) {
+        return (isUpEdge(threadRow, threadCol)
+        || isRightEdge(threadRow, threadCol)
+        || isDownEdge(threadRow, threadCol)
+        || isLeftEdge(threadRow, threadCol));
+    }
+
+    private boolean isUpEdge(int threadRow, int threadCol) {
+        return (!isThreadIsCorner(threadRow, threadCol) && threadRow == 0);
+    }
+
+    private boolean isRightEdge(int threadRow, int threadCol) {
+        return (!isThreadIsCorner(threadRow, threadCol) && threadCol == this.vSplit - 1);
+    }
+
+    private boolean isDownEdge(int threadRow, int threadCol) {
+        return (!isThreadIsCorner(threadRow, threadCol) && threadRow == this.hSplit - 1);
+    }
+
+    private boolean isLeftEdge(int threadRow, int threadCol) {
+        return (!isThreadIsCorner(threadRow, threadCol) && threadCol == 0);
+    }
+
+    private boolean isThreadIsCorner(int threadRow, int threadCol) {
+        return (isUpRightCorner(threadRow, threadCol)
+        || isDownRightCorner(threadRow, threadCol)
+        || isDownLeftCorner(threadRow, threadCol)
+        || isUpLeftCorner(threadRow, threadCol));
+    }
+
+    private boolean isUpRightCorner(int threadRow, int threadCol) {
+        return (threadRow == 0 && threadCol == this.vSplit - 1);
+    }
+
+    private boolean isDownRightCorner(int threadRow, int threadCol) {
+        return (threadRow == this.hSplit - 1 && threadCol == this.vSplit - 1);
+    }
+
+    private boolean isDownLeftCorner(int threadRow, int threadCol) {
+        return (threadRow == this.hSplit - 1 && threadCol == 0);
+    }
+
+    private boolean isUpLeftCorner(int threadRow, int threadCol) {
+        return (threadRow == 0 && threadCol == 0);
     }
 }
