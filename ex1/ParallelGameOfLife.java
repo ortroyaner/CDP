38561bdd288b0be4e6200698fcdde90879d2e678
Or Troyaner <or.troyaner@gmail.com>
2017-11-13 19:10:07 +0200

    parallel start and join function

--- ex1/ParallelGameOfLife.java
+++ ex1/ParallelGameOfLife.java
@@ -4,41 +4,58 @@ import java.util.ArrayList;
 
 public class ParallelGameOfLife implements GameOfLife {
 
-	public boolean[][][] invoke(boolean[][] initalField, int hSplit, int vSplit,
-			int generations) {
+    public boolean[][][] invoke(boolean[][] initialField, int hSplit, int vSplit,
+                                int generations) {
         // tables to return
         boolean[][] beforeLastTable;
         boolean[][] lastTable;
 
         // create singleThreaded games
-		int numOfRows = initalField.length;
-		int numOfCols = initalField[0].length;
-		int rowJump = numOfRows/vSplit; int colJump = numOfCols/hSplit;
-		int countC = 0; int countR = 0;
+        int numOfRows = initialField.length;
+        int numOfCols = initialField[0].length;
+        int rowJump = numOfRows / vSplit;
+        int colJump = numOfCols / hSplit;
+        int countC = 0;
+        int countR = 0;
 
         Thread[][] threads = new Thread[vSplit][hSplit];
 
         // give data to ThreadsCommunicator , hSplit + vSplit
-		//TODO: create communicator and send to singleThread
-		ThreadsCommunicator communicator = new ThreadsCommunicator(vSplit,hSplit);
+        //TODO: create communicator and send to singleThread
+        ThreadsCommunicator communicator = new ThreadsCommunicator(vSplit, hSplit);
 
-        for(int i=0; i<numOfRows && countR < vSplit; i+=rowJump){
-			for(int j=0; j<numOfCols && countC < hSplit; j+=colJump){
-				// [i][j] is the mini section to solve by one thread
-				// create for every [i][j] a SingleThreadGoL
-                threads[i][j] = new Thread(new SingleThreadGameOfLife(initalField, new Index(i,j),
-                        new Index(i+rowJump,j+colJump),countR , countC, numOfRows, numOfCols,generations, communicator));
+        for (int i = 0; i < numOfRows && countR < vSplit; i += rowJump) {
+            for (int j = 0; j < numOfCols && countC < hSplit; j += colJump) {
+                // [i][j] is the mini section to solve by one thread
+                // create for every [i][j] a SingleThreadGoL
+                threads[i][j] = new Thread(new SingleThreadGameOfLife(initialField, new Index(i, j),
+                        new Index(i + rowJump, j + colJump), countR, countC, numOfRows, numOfCols, generations, communicator));
+                countC++;
+            }
+            countC = 0;
+            countR++;
+        }
 
+        startThreadsMatrixAndThenJoin(vSplit, hSplit, threads);
 
-				countC++;
-			}
-			countC=0;
-			countR++;
-		}
+        return null;
+    }
 
-		return null;
-	}
-
-	//TODO: method that will start all threads, and will join them afterwards
+    private void startThreadsMatrixAndThenJoin(int numOfRows, int numOfCols, Thread[][] threads) {
+        for (int i = 0; i < numOfRows; i++) {
+            for (int j = 0; j < numOfCols; j++) {
+                threads[i][j].start();
+            }
+        }
+        for (int i = 0; i < numOfRows; i++) {
+            for (int j = 0; j < numOfCols; j++) {
+                try {
+                    threads[i][j].join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
     //TODO: fill out tables to return
 }
